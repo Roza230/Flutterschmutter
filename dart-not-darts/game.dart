6173977f67a2b896ab5086b96949f6d34f1ab2e9
Roza Antonevici <rozic.antonevici@me.com>
2025-05-20 08:45:10 +0200

    Update game.dart

--- dart-not-darts/game.dart
+++ dart-not-darts/game.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          '🎵 Game Starts Here!',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
